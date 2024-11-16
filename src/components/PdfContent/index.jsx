21087916cee7c1f6e6cd69eeaed4bf4ef6e35f56
Fabio Martins <fabio91.dev@gmail.com>
2024-11-16 13:17:05 -0300

    implementado quantidade de linhas em função da quantidade de disciplinas

--- src/components/PdfContent/index.jsx
+++ src/components/PdfContent/index.jsx
@@ -2,15 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
 
-
-// Função para buscar o caminho da imagem do backend
 const fetchImage = async () => {
   try {
-    const response = await fetch('/api/getImagePath'); // Chama a API para pegar o caminho da imagem
+    const response = await fetch('/api/getImagePath');
     const data = await response.json();
-    
     if (data.success) {
-      
       return data.imgBase64;
     } else {
       console.error('Error fetching image path:', data.message);
@@ -22,81 +18,71 @@ const fetchImage = async () => {
   }
 };
 
-
 const PdfGenerator = ({ updatedButtonValues, coordenadores, diretores, turma, cabecalho, tipoEnsino, conceitoFinal, disciplinas, date }) => {
   const [imageBase64, setImageBase64] = useState(null);
   const [localUpdateButtonValues, setLocalUpdateButtonValues] = useState(updatedButtonValues);
-  
-  // useEffect para atualizar localUpdateButtonValues quando updatedButtonValues mudar
+
   useEffect(() => {
     setLocalUpdateButtonValues(updatedButtonValues);
   }, [updatedButtonValues]);
 
-  
-
   useEffect(() => {
     const loadImage = async () => {
       try {
-        const image64 = await fetchImage(); // Pega o caminho da imagem
-        
+        const image64 = await fetchImage();
         if (image64) {
-          setImageBase64(image64); // Define o estado da imagem
+          setImageBase64(image64);
         }
       } catch (error) {
         console.error('Erro ao carregar a imagem:', error);
       }
     };
-
-    loadImage(); // Carrega a imagem quando o componente é montado
-  }, []); // Dependência vazia para garantir que execute apenas uma vez
+    loadImage();
+  }, []);
 
   const handleGeneratePDF = () => {
     const dataAtual = new Date();
     const anoAtual = dataAtual.getFullYear();
-    const opcoes = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const dataTextoFormatada = dataAtual.toLocaleDateString('pt-BR', opcoes);
-  
     const generatePDFFileName = () => {
       const now = new Date();
       const date = now.toLocaleDateString().replace(/\//g, '-');
       const time = now.toLocaleTimeString().replace(/:/g, '-');
       return `Ata_conselho_${date}_${time}.pdf`;
     };
-  
+
     let nomeEscola = '';
     let bimestre = '';
     let cidade = '';
     let tipoEnsino_parse = '';
-  
+
     if (tipoEnsino) {
       tipoEnsino_parse = tipoEnsino.nome_tipo_ensino;
     }
-  
+
     cabecalho.forEach((item) => {
       nomeEscola = item.nome;
       bimestre = item.bimestre;
       cidade = item.cidade;
     });
-  
+
     const doc = new jsPDF({
       orientation: 'landscape',
       unit: 'pt',
       format: 'a4',
     });
-  
-    // Configurar fontes e adicionar imagem
+
     doc.setFont('Arial', 'bold', 20);
-  
+
     if (imageBase64) {
       doc.addImage(imageBase64, 'PNG', 50, 40, 50, 50);
     } else {
       console.error('Imagem não carregada corretamente');
     }
-  
+
     doc.text(`${nomeEscola}`, doc.internal.pageSize.getWidth() / 2, 40, { align: 'center' });
     doc.text(`${cidade}`, doc.internal.pageSize.getWidth() / 2, 70, { align: 'center' });
     doc.text('ATA DO CONSELHO DE CLASSE E SÉRIE', doc.internal.pageSize.getWidth() / 2, 100, { align: 'center' });
-  
+
     if (conceitoFinal) {
       doc.text(`CONCEITO FINAL DE ${anoAtual} - ${tipoEnsino_parse}`, doc.internal.pageSize.getWidth() / 2, 140, { align: 'center' });
       doc.text(` ${turma} -`, doc.internal.pageSize.getWidth() / 2, 160, { align: 'center' });
@@ -104,13 +90,13 @@ const PdfGenerator = ({ updatedButtonValues, coordenadores, diretores, turma, ca
       doc.text(`${bimestre}º BIMESTRE DE ${anoAtual} - ${tipoEnsino_parse}`, doc.internal.pageSize.getWidth() / 2, 140, { align: 'center' });
       doc.text(` ${turma}`, doc.internal.pageSize.getWidth() / 2, 160, { align: 'center' });
     }
-  
+
     doc.setFontSize(12);
     doc.text(`Na data de, ${date}, reuniram-se os Professores e o Coordenador Pedagógico`, doc.internal.pageSize.getWidth() / 2, 180, { align: 'center' });
     doc.text(`para a realização do Conselho de Classe e Série, na forma presencial, com a presença registrada abaixo.`, doc.internal.pageSize.getWidth() / 2, 200, { align: 'center' });
-  
+
     doc.text(`Legendas: NF:  Nota e Falta | N:  Nota abaixo da média | F: Frequência abaixo de 75%  | A: Aprovado | R: Reprovado | AC: Aprovado pelo Conselho`, doc.internal.pageSize.getWidth() / 2, 235, { align: 'center' });
-  
+
     // Criar cabeçalho da tabela
     let columns = ['Nome'];
     if (conceitoFinal) {
@@ -126,11 +112,6 @@ const PdfGenerator = ({ updatedButtonValues, coordenadores, diretores, turma, ca
       columns = columns.concat(disciplinas.sort());
     }
 
-    // Log para verificação
-    console.log("Colunas geradas:", columns);
-    console.log("localUpdateButtonValues:", localUpdateButtonValues);
-    
-  
     // Criar linhas da tabela com dados dos alunos
     const rows = [];
     const students = Object.keys(localUpdateButtonValues);
@@ -141,17 +122,21 @@ const PdfGenerator = ({ updatedButtonValues, coordenadores, diretores, turma, ca
       }
       rows.push(row);
     }
-  
+
     // Função para dividir as linhas entre a primeira página e as demais
-    const splitRowsByPage = (rows) => {
-      const firstPageRows = rows.slice(0, 6); // Primeira página com 4 linhas
-      const otherPageRows = rows.slice(8); // Demais linhas
-      return [firstPageRows, otherPageRows];
+    const splitRowsByPage = (rows, disciplinasLength) => {
+      const firstPageRowsCount = disciplinasLength <= 13 ? 12 : 6;
+      const subsequentPageRowsCount = disciplinasLength <= 13 ? 19 : 10;
+
+      const firstPageRows = rows.slice(0, firstPageRowsCount);
+      const otherPageRows = rows.slice(firstPageRowsCount);
+
+      return { firstPageRows, otherPageRows, subsequentPageRowsCount };
     };
-  
-    const [firstPageRows, otherPageRows] = splitRowsByPage(rows);
-  
-    // Adicionar a tabela da primeira página com 4 linhas
+
+    const { firstPageRows, otherPageRows, subsequentPageRowsCount } = splitRowsByPage(rows, disciplinas.length);
+
+    // Adicionar a tabela da primeira página
     doc.autoTable({
       theme: 'grid',
       startY: 250,
@@ -174,18 +159,18 @@ const PdfGenerator = ({ updatedButtonValues, coordenadores, diretores, turma, ca
         0: { fontStyle: 'bold', cellWidth: 'auto', fillColor: [220, 220, 220] },
       },
     });
-  
-    // Adicionar as páginas seguintes com 6 linhas por página
+
+    // Adicionar as páginas seguintes
     let remainingRows = otherPageRows;
     while (remainingRows.length > 0) {
-      doc.addPage(); // Nova página
-  
-      const rowsForThisPage = remainingRows.slice(0, 10);
-      remainingRows = remainingRows.slice(15);
-  
+      doc.addPage();
+
+      const rowsForThisPage = remainingRows.slice(0, subsequentPageRowsCount);
+      remainingRows = remainingRows.slice(subsequentPageRowsCount);
+
       doc.autoTable({
         theme: 'grid',
-        startY: 50, // Ajuste conforme necessário
+        startY: 50,
         head: [columns],
         body: rowsForThisPage,
         styles: {
@@ -206,33 +191,32 @@ const PdfGenerator = ({ updatedButtonValues, coordenadores, diretores, turma, ca
         },
       });
     }
-  
+
     // Adicionar uma nova página para as assinaturas
     doc.addPage();
-  
+
     doc.text(`Assinaturas dos professores, coordenação e direção responsáveis pela turma:`, doc.internal.pageSize.getWidth() / 2, 40, { align: 'center' });
     doc.text(` ${turma} - ${tipoEnsino_parse}`, doc.internal.pageSize.getWidth() / 2, 60, { align: 'center' });
-  
+
     doc.setFontSize(10);
     doc.text('PROFESSORES(AS)', 150, 100);
     doc.text('ASSINATURA', 300, 100);
-  
+
     for (let i = 0; i < 20; i++) {
       const lineY = 120 + 10 + i * 15;
-      doc.line(100, lineY, 270, lineY); // Linha horizontal para professores
-      doc.line(300, lineY, 390, lineY); // Linha horizontal para assinaturas
+      doc.line(100, lineY, 270, lineY);
+      doc.line(300, lineY, 390, lineY);
     }
-  
+
     doc.text(`COORDENAÇÃO: ${coordenadores}`, 500, 100);
     doc.text('______________________', 550, 120);
     doc.text(`DIREÇÃO: ${diretores}`, 500, 170);
     doc.text('______________________', 550, 190);
-  
+
     doc.text(`${bimestre}º BIMESTRE DE ${anoAtual} - ${turma} - ${tipoEnsino_parse}`, doc.internal.pageSize.getWidth() / 2, 570, { align: 'center' });
-  
+
     doc.save(generatePDFFileName());
   };
-  
 
   return (
     <div className="text-center mt-4">
